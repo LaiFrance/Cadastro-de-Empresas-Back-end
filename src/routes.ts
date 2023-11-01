729a0379d9083fa4421cdad7fa0cc679b4820232
LaiFrance <laise94@hotmail.com>
2023-11-01 03:59:14 -0300

    feat(api-client): Add library for HTTP requests

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,42 @@ import {Router, Request, Response } from 'express';
 const router = Router();
 
 router.get('/teste', (req: Request, res: Response) => {
-  res.send('Hello World');
+  throw new Error('Erro teste'); 
 });
+
+// Rota para criar uma nova empresa
+router.post('/empresa', (req: Request, res: Response) => {
+  // Lógica para criar uma nova empresa
+  res.send('Rota POST /empresa: Criar uma nova empresa');
+});
+
+// Rota para listar todas as empresas cadastradas
+router.get('/empresas', (req: Request, res: Response) => {
+  // Lógica para listar todas as empresas cadastradas
+  res.send('Rota GET /empresas: Listar todas as empresas cadastradas');
+});
+
+// Rota para consultar uma empresa específica por CNPJ
+router.get('/empresa/:cnpj', (req: Request, res: Response) => {
+  const cnpj = req.params.cnpj;
+  // Lógica para consultar uma empresa por CNPJ
+  res.send(`Rota GET /empresa/${cnpj}: Consultar empresa por CNPJ`);
+});
+
+// Rota para atualizar os dados de uma empresa
+router.put('/empresa/:cnpj', (req: Request, res: Response) => {
+  const cnpj = req.params.cnpj;
+  // Lógica para atualizar os dados de uma empresa por CNPJ
+  res.send(`Rota PUT /empresa/${cnpj}: Atualizar dados da empresa`);
+});
+
+// Rota para excluir uma empresa
+router.delete('/empresa/:cnpj', (req: Request, res: Response) => {
+  const cnpj = req.params.cnpj;
+  // Lógica para excluir uma empresa por CNPJ
+  res.send(`Rota DELETE /empresa/${cnpj}: Excluir empresa`);
+});
+
+
 
 export { router}
