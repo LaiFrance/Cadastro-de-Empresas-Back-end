5c74da9f09a8fb8e4655cfaae179aff6aab9ad31
LaiFrance <laise94@hotmail.com>
2023-11-01 22:40:18 -0300

    feat: Implement JWT (JSON Web Token) authentication

--- src/services/auth/AuthService.ts
+++ src/services/auth/AuthService.ts
@@ -1,42 +1,56 @@
-import PrismaClient  from "../../prisma"
+import PrismaClient from "../../prisma";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 interface AuthRequest {
-    email: string;
-    senha: string;
+  email: string;
+  senha: string;
 }
 
 class AuthUserService {
-    async execute({ email, senha }: AuthRequest) {
-        if(!email || !senha){
-            throw new Error("Missing information");
-        }
-
-        // Verificar se o usuário existe
-        const user = await PrismaClient.usuario.findFirst({
-            where: {
-                email
-            }
-        });
-
-        if(!user){
-        throw new Error("Email or password incorrect" );
-        }
-
-        // Verificar se a senha está correta
-        const passwordMatch = await bcrypt.compare(senha, user.senha);
-
-        if(!passwordMatch){
-            throw new Error("Email or password incorrect");
-        }
-
-        else{
-            return ('Login realizado com sucesso');
-
+  async execute({ email, senha }: AuthRequest) {
+    if (!email || !senha) {
+      throw new Error("Missing information");
     }
 
-}
+    // Verificar se o usuário existe
+    const user = await PrismaClient.usuario.findFirst({
+      where: {
+        email
+      }
+    });
 
+    if (!user) {
+      throw new Error("Email or password incorrect");
+    }
+
+    // Verificar se a senha está correta
+    const passwordMatch = await bcrypt.compare(senha, user.senha);
+
+    if (!passwordMatch) {
+      throw new Error("Email or password incorrect");
+    }
+
+    // Gerar o token
+    const token = jwt.sign(
+      {
+        nome: user.nome,
+        email: user.email
+      },
+      process.env.JWT_SECRET,
+      {
+        subject: user.id.toString(),
+        expiresIn: "30d"
+      }
+    );
+
+    return {
+      id: user.id,
+      nome: user.nome,
+      email: user.email,
+      token: token
+    };
+  }
 }
 
 export { AuthUserService };
